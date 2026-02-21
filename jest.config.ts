dadeed6f8a6e87677c7df3fde967a93815bfbad0
Wilfried <wilfried@lucha.at>
2026-02-21 08:13:44 +0100

    chore: fix jest and tsconfig for Angular 21

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,15 @@ import type { Config } from 'jest';
 const jestConfig: Config = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
+  },
 };
 
 export default jestConfig;
