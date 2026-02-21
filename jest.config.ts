2b970aacf01417941f9fc1ebb448fabba2e00a47
Wilfried <wilfried@lucha.at>
2026-02-21 08:52:53 +0100

    chore: Update dependencies, configure Jest transform ignore

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'jest';
 const jestConfig: Config = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$|@jsverse)'],
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
