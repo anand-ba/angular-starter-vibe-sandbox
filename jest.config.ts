3ce9fc36a6d43c8aa5e70fd2c50851a472563780
Wilfried <wilfried@lucha.at>
2026-02-21 09:06:27 +0100

    chore: add testPathIgnorePatterns to Jest configuration

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'jest';
 const jestConfig: Config = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/tests/'],
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$|@jsverse)'],
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
